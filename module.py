48593c0f42540d1e7514311ffd355d442f9980f8
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:11 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,29 +1,29 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'voip'
-    MODULE_NAME = _('VoIP & Telephony')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'call-outline'
-    MODULE_DESCRIPTION = _('VoIP integration, call logging and telephony')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'communication'
+MODULE_ID = 'voip'
+MODULE_NAME = _('VoIP & Telephony')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'call-outline'
+MODULE_DESCRIPTION = _('VoIP integration, call logging and telephony')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'communication'
 
-    MENU = {
-        'label': _('VoIP & Telephony'),
-        'icon': 'call-outline',
-        'order': 66,
-    }
+MENU = {
+    'label': _('VoIP & Telephony'),
+    'icon': 'call-outline',
+    'order': 66,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Call Log'), 'icon': 'call-outline', 'id': 'calls'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'voip.view_calllog',
+PERMISSIONS = [
+    'voip.view_calllog',
 'voip.add_calllog',
 'voip.manage_settings',
-    ]
+]
