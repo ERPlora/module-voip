591343ea0cd6478863e21a0b0d67229fbf53f612
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:25 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ VoIP & Telephony Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -113,6 +115,7 @@ def call_logs_list(request):
     }
 
 @login_required
+@htmx_view('voip/pages/call_log_add.html', 'voip/partials/call_log_add_content.html')
 def call_log_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -136,10 +139,13 @@ def call_log_add(request):
         obj.recording_url = recording_url
         obj.notes = notes
         obj.save()
-        return _render_call_logs_list(request, hub_id)
-    return django_render(request, 'voip/partials/panel_call_log_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('voip:call_logs_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('voip/pages/call_log_edit.html', 'voip/partials/call_log_edit_content.html')
 def call_log_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(CallLog, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -155,7 +161,7 @@ def call_log_edit(request, pk):
         obj.notes = request.POST.get('notes', '').strip()
         obj.save()
         return _render_call_logs_list(request, hub_id)
-    return django_render(request, 'voip/partials/panel_call_log_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
